040039ba64a163c16534a1999523cd903d9a0dbf
Collin Wilkins <cwilki32@Collins-MBP.attlocal.net>
2022-02-17 12:59:27 -0500

    Adding exercise 12.1, using this. modifier

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -3,28 +3,34 @@ package com.company;
 public class Car {
     //Define your Car class with at least (3) instance variables such as color,
     // make, model, etc., and create accessor method for each variable
-    private String vehColor;
+//    private String vehColor; removed for example
     private String vehMake;
     private String vehModel;
 
-    // uses accessor methods to set each of its instance variables and then gets their values,
+    // uses accessor methods to set each of its instance variables and then gets their values, still using "this"
+    //for if I don't want to parameterize
 
     //accessor setter methods
-    public void setVehColor(String color) {
-        vehColor = color;
+ /*   public void setVehColor(String vehColor) {
+        this.vehColor = vehColor;
     }
-    public void setVehMake(String make) {
-        vehMake = make;
+    public void setVehMake(String vehMake) {
+        this.vehMake = vehMake;
     }
-    public void setVehModel(String model) {
-        vehModel = model;
+    public void setVehModel(String vehModel) {
+        this.vehModel = vehModel;
     }
-
+*/
+    //constructor
+    public Car(String vehMake, String vehModel) {//using new constructor and this. modifier
+        this.vehMake = vehMake;
+        this.vehModel = vehModel;
+    }
     //accessor getter methods
 
-    public String getVehColor() {
-        return vehColor;
-    }
+ //   public String getVehColor() { old method removed for example
+ //       return vehColor;
+ //   }
 
     public String getVehMake() {
         return vehMake;
--- src/com/company/CarDemo.java
+++ src/com/company/CarDemo.java
@@ -6,22 +6,21 @@ public class CarDemo {
     //Create a new Java class file (File -> New -> Java Class) named Car
     //Define your Car class with at least (3) instance variables such as color,
     // make, model, etc., and create accessor method for each variable
-    //In your CarDemo class, write a program that instantiates a Car object named myCar,
+    // In your CarDemo class, write a program that instantiates a Car object named myCar,
     // uses accessor methods to set each of its instance variables and then gets their values,
     // and finally prints out information about your vehicle
 
     public static void main(String[] args) {
 	// write your code here
         // create a car object named myCar
-        Car myCar = new Car();
+        Car myCar = new Car("Ford", "Bronco");
 
         //set each instance variable and then gets their values
-        myCar.setVehColor("Gray"); //color of car is blue
-        myCar.setVehMake("Ford"); //make is Ford
-        myCar.setVehModel("F150"); //model is F150
+//        myCar.setVehColor("Gray"); //color of car is blue
+//        myCar.setVehMake("Ford"); //make is Ford
+//        myCar.setVehModel("F150"); //model is F150
 
-        System.out.println("I drive a " + myCar.getVehColor() + " " + myCar.getVehMake() + " " +
-                myCar.getVehModel());
-
+        System.out.println("I wish I drove a" + " " + myCar.getVehMake() + " " +
+                myCar.getVehModel() + ".");
     }
 }
